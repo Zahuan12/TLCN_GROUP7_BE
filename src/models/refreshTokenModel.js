b9dbb0e76a6fd7379eda2316acc688dcd24d317e
Trung <dinhtrung0012@gmail.com>
2025-09-27 11:02:48 +0700

    fix logout func and model

--- src/models/refreshTokenModel.js
+++ src/models/refreshTokenModel.js
@@ -5,8 +5,25 @@ module.exports = (sequelize, DataTypes) => {
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true
     },
-    token: { type: DataTypes.STRING, allowNull: false },
-    expiresAt: { type: DataTypes.DATE, allowNull: false }
+    token: { 
+      type: DataTypes.STRING, 
+      allowNull: false 
+    },
+    expiresAt: { 
+      type: DataTypes.DATE, 
+      allowNull: false 
+    },
+    // 👇 Thêm khóa ngoại userId
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'users', // Tên bảng User
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    }
   }, {
     tableName: 'refresh_tokens'
   });
